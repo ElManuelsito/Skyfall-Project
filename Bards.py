341e3104ccc202f926a8d856608d12f23b097644
Eber13 <148716931+Eber13@users.noreply.github.com>
2023-10-28 18:39:24 -0300

    Add files via upload

---
+++ Bards.py
@@ -0,0 +1,22 @@
+from Character import Character
+
+# class Bard(Character):
+#     def __init__(self, name):
+#         super().__init__(name, health, armor, str, agi, int, faith,
+#                  acc, res_magic, res_phys, money, lvl, exp, armor_items_on, weapon_items_on)
+
+
+class Bard(Character):
+    def __init__(self,name):
+        super().__init__(name, health=80, armor=10, str=5, agi=6, int=30, faith=10, acc=14, res_magic=6,
+                         res_phys=4, money=0, lvl=0, exp=0, armor_items_on=0, weapon_items_on=0)
+
+    def skill_a(self):
+        pass #Helear
+
+    def skill_b(self):
+        pass #De buff de veneno 
+
+
+player = Bard("Saint.e")
+player.skill_b()
