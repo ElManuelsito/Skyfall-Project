4d064365086566911d0d43d4074c5eead9195f11
Eber13 <148716931+Eber13@users.noreply.github.com>
2023-10-28 18:55:21 -0300

    Update Wizard.py

--- Wizard.py
+++ Wizard.py
@@ -6,7 +6,7 @@ from Character import Character
 #                  acc, res_magic, res_phys, money, lvl, exp, armor_items_on, weapon_items_on)
 
 
-class Mage(Character):
+class Wizard(Character):
     def __init__(self,name):
         super().__init__(name, health=100, armor=15, str=10, agi=6, int=30, faith=6, acc=15, res_magic=6,
                          res_phys=4, money=0, lvl=0, exp=0, armor_items_on=0, weapon_items_on=0)
