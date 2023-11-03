dd47d96fc705fba9339d246878ca3aa15cb5d9e0
Eber13 <148716931+Eber13@users.noreply.github.com>
2023-11-03 20:25:14 -0300

    Update Wizard.py

--- ClassesRPG/Wizard.py
+++ ClassesRPG/Wizard.py
@@ -8,7 +8,7 @@ from Character import Character
 
 class Wizard(Character):
     def __init__(self, name):
-        super().__init__(name, health=100, armor=15, mana=75, str=10, agi=6, int=30, faith=6, acc=15, res_magic=10,
+        super().__init__(name, health=100, armor=15, mana=100, str=10, agi=6, int=30, faith=6, acc=15, res_magic=10,
                          res_phys=3, money=0, lvl=0, exp=0, armor_items_on=0, weapon_items_on=0)
 
     def skill_a(self):
