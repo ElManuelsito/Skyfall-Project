e4d90cdf567050d851543d5407f60257c947afbe
ElManuelsito <mmonsalve1357@gmail.com>
2023-11-05 20:51:08 -0300

    Add calculateEnemyHealth

--- Enemy.py
+++ Enemy.py
@@ -7,7 +7,6 @@ import ClassesRPG.Necromancer as Cl_Necro
 import ClassesRPG.Paladin as Cl_Paladin
 import ClassesRPG.Sorcerer as Cl_Sorcerer
 import ClassesRPG.Warrior as Cl_Warrior
-import Constants
 from Skyfall import Manager
 
 
@@ -20,94 +19,48 @@ class Enemy:
     def attack(self, player):
         player.takeDamage(self)
 
-    def takeDamageFromPlayerPrimary(self, player_classobj, player_info_dictobj):
-        if type(player_classobj) == Cl_Archer.Archer:
-            if Manager.GameManager.calculateChance((70 + ((70 * player_classobj.acc)/100))):
-                final_health = self.health - (player_info_dictobj["weapon_1"].dmg + ((player_info_dictobj["weapon_1"] * player_classobj.acc)/200))
-                self.health = float(f"{final_health:.2f}")
-                if self.health <= 0:
-                    return self.isDead()
-            else:
-                Manager.showCombinedMessage(player_classobj.name, Constants.SOMEONE_ATTACKS_AND_MISSES)
+    def takeDamageFromPlayerCommon(self, player_classobj):
+        if type(player_classobj) is Cl_Archer.Archer:
+            self.health = Manager.calculateEnemyHealthAfterPlayerAttack(player_classobj.acc, self.health, 200)
+            if self.health <= 0:
+                return self.isDead()
+        elif type(player_classobj) is Cl_Bard.Bard:
+            self.health = Manager.calculateEnemyHealthAfterPlayerAttack(player_classobj.acc, self.health, 150)
+            if self.health <= 0:
+                return self.isDead()
+        elif type(player_classobj) is Cl_Necro.Necromancer:
+            self.health = Manager.calculateEnemyHealthAfterPlayerAttack(player_classobj.acc, self.health, 200)
+            if self.health <= 0:
+                return self.isDead()
+        elif type(player_classobj) is Cl_Paladin.Paladin:
+            self.health = Manager.calculateEnemyHealthAfterPlayerAttack(player_classobj.acc, self.health, 150)
+            if self.health <= 0:
+                return self.isDead()
+        elif type(player_classobj) is Cl_Sorcerer.Sorcerer:
+            self.health = Manager.calculateEnemyHealthAfterPlayerAttack(player_classobj.acc, self.health, 200)
+            if self.health <= 0:
+                return self.isDead()
+        elif type(player_classobj) is Cl_Tank.Tank:
+            self.health = Manager.calculateEnemyHealthAfterPlayerAttack(player_classobj.acc, self.health, 200)
+            if self.health <= 0:
+                return self.isDead()
+        elif type(player_classobj) is Cl_Thief.Thief:
+            self.health = Manager.calculateEnemyHealthAfterPlayerAttack(player_classobj.acc, self.health, 150)
+            if self.health <= 0:
+                return self.isDead()
+        elif type(player_classobj) is Cl_Warrior.Warrior:
+            self.health = Manager.calculateEnemyHealthAfterPlayerAttack(player_classobj.acc, self.health, 200)
+            if self.health <= 0:
+                return self.isDead()
+        elif type(player_classobj) is Cl_Wizard.Wizard:
+            self.health = Manager.calculateEnemyHealthAfterPlayerAttack(player_classobj.acc, self.health, 180)
+            if self.health <= 0:
+                return self.isDead()
 
-        elif type(player_classobj)==Cl_Bard.Bard:
-            if Manager.GameManager.calculateChance((Constants.BASE_ACC+((Constants.BASE_ACC* player_classobj.acc)/100))):
-                final_health = self.health - (player_info_dictobj["weapon_1"].dmg + ((player_info_dictobj["weapon_1"] * player_classobj.int)/150))
-                self.health = float(f"{final_health:.2f}")
-                if self.health <= 0:
-                    return self.isDead()
-            else:
-                Manager.showCombinedMessage(player_classobj.name, Constants.SOMEONE_ATTACKS_AND_MISSES)
-
-        elif type(player_classobj)==Cl_Necro.Necromancer:
-            if Manager.GameManager.calculateChance((Constants.BASE_ACC+((Constants.BASE_ACC* player_classobj.acc)/100))):
-                final_health = self.health - (player_info_dictobj["weapon_1"].dmg + ((player_info_dictobj["weapon_1"] * player_classobj.int)/200))
-                self.health = float(f"{final_health:.2f}")
-                if self.health <= 0:
-                    return self.isDead()
-            else:
-                Manager.showCombinedMessage(player_classobj.name, Constants.SOMEONE_ATTACKS_AND_MISSES)
-
-        elif type(player_classobj)==Cl_Paladin.Paladin:
-            if Manager.GameManager.calculateChance((Constants.BASE_ACC+((Constants.BASE_ACC* player_classobj.acc)/100))):
-                final_health = self.health - (player_info_dictobj["weapon_1"].dmg + ((player_info_dictobj["weapon_1"] * player_classobj.str)/150))
-                self.health = float(f"{final_health:.2f}")
-                if self.health <= 0:
-                    return self.isDead()
-            else:
-                Manager.showCombinedMessage(player_classobj.name, Constants.SOMEONE_ATTACKS_AND_MISSES)
-
-        elif type(player_classobj)==Cl_Sorcerer.Sorcerer:
-            if Manager.GameManager.calculateChance((Constants.BASE_ACC+((Constants.BASE_ACC* player_classobj.acc)/100))):
-                final_health = self.health - (player_info_dictobj["weapon_1"].dmg + ((player_info_dictobj["weapon_1"] * player_classobj.int)/200))
-                self.health = float(f"{final_health:.2f}")
-                if self.health <= 0:
-                    return self.isDead()
-            else:
-                Manager.showCombinedMessage(player_classobj.name, Constants.SOMEONE_ATTACKS_AND_MISSES)
-
-        elif type(player_classobj)==Cl_Tank.Tank:
-            if Manager.GameManager.calculateChance((Constants.BASE_ACC+((Constants.BASE_ACC* player_classobj.acc)/100))):
-                final_health = self.health - (player_info_dictobj["weapon_1"].dmg + ((player_info_dictobj["weapon_1"] * player_classobj.str)/200))
-                self.health = float(f"{final_health:.2f}")
-                if self.health <= 0:
-                    return self.isDead()
-            else:
-                Manager.showCombinedMessage(player_classobj.name, Constants.SOMEONE_ATTACKS_AND_MISSES)
-
-        elif type(player_classobj)==Cl_Thief.Thief:
-            if Manager.GameManager.calculateChance((Constants.BASE_ACC+((Constants.BASE_ACC* player_classobj.acc)/100))):
-                final_health = self.health - (player_info_dictobj["weapon_1"].dmg + ((player_info_dictobj["weapon_1"] * player_classobj.str)/150))
-                self.health = float(f"{final_health:.2f}")
-                if self.health <= 0:
-                    return self.isDead()
-            else:
-                Manager.showCombinedMessage(player_classobj.name, Constants.SOMEONE_ATTACKS_AND_MISSES)
-
-        elif type(player_classobj)==Cl_Warrior.Warrior:
-            if Manager.GameManager.calculateChance((Constants.BASE_ACC+((Constants.BASE_ACC* player_classobj.acc)/100))):
-                final_health = self.health - (player_info_dictobj["weapon_1"].dmg + ((player_info_dictobj["weapon_1"] * player_classobj.str)/200))
-                self.health = float(f"{final_health:.2f}")
-                if self.health <= 0:
-                    return self.isDead()
-            else:
-                Manager.showCombinedMessage(player_classobj.name, Constants.SOMEONE_ATTACKS_AND_MISSES)
-
-        elif type(player_classobj)==Cl_Wizard.Wizard:
-            if Manager.GameManager.calculateChance((Constants.BASE_ACC+((Constants.BASE_ACC* player_classobj.acc)/100))):
-                final_health = self.health - (player_info_dictobj["weapon_1"].dmg + ((player_info_dictobj["weapon_1"] * player_classobj.int)/180))
-                self.health = float(f"{final_health:.2f}")
-                if self.health <= 0:
-                    return self.isDead()
-            else:
-                Manager.showCombinedMessage(player_classobj.name, Constants.SOMEONE_ATTACKS_AND_MISSES)
-    def takeDamageFromPlayerSpecial(self, player_classobj, selected_skill):
-        if type(player_classobj) == Cl_Archer.Archer:
-            if selected_skill == "A"
-                final_healt = self.health-(player_info_dictobj)]
-                pass
-            elif == "B"
-            elif == "C"
+    def takeDamageFromPlayerSpecial(self, player_classobj):
+        if type(player_classobj) is Cl_Archer.Archer:
+            # final_healt =  calculateEnemeyHealht()? u otro metodo?
+            pass
 
     def isDead(self):
         return True
